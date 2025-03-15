8654959ccc11fdf9cbecb9e78bfe2303c3e22242
paxington181 <tsandrus@rogers.com>
2025-03-15 13:50:14 -0400

    Entry boxes and issues between pack and grid

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ m.title("This is a triumpth")
 m_label = Label(m, text = "I'm making a note here")
 m_label.pack()
 
+
+
 button = tk.Button(m, text = "Huge success", width = 30, command = m.destroy)
 button.pack()
+
+entry_window = Toplevel(m)
+
+Label(entry_window, text = "Glad").grid(row = 0)
+Label(entry_window, text = "OS").grid(row = 1)
+user_entry_1 = Entry(entry_window)
+user_entry_2 = Entry(entry_window)
+user_entry_1.grid(row = 0, column = 1)
+user_entry_2.grid(row = 1, column = 1)
+
+
 
 m.mainloop()
