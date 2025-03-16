69c54f94b0aeabbdc0af0d2612a4451ecef88b34
paxington181 <tsandrus@rogers.com>
2025-03-16 10:54:17 -0400

    Scrollbar done

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ m_label.pack()
 button = tk.Button(m, text = "Huge success", width = 30, command = m.destroy)
 button.pack()
 
+
 portal = IntVar()
 portal_2 = IntVar()
 Radiobutton(m, text = "Red portal", variable = portal, value = 1).pack(anchor = W)
 Radiobutton(m, text = "Blue portal", variable = portal, value = 2).pack(anchor=W)
 Radiobutton(m, text = "Green portal", variable = portal_2, value = 3).pack(anchor = W)
 Radiobutton(m, text = "Yellow portal", variable = portal_2, value = 4).pack(anchor = W)
+
 
 Lb = Listbox(m)
 Lb.insert(0, "Index 0")
@@ -28,11 +30,28 @@ Lb.insert(4, "Index 4")
 Lb.insert(5, "Index 5")
 Lb.pack()
 
+
+scrollbar = Scrollbar(m)
+scrollbar.pack(side = RIGHT, fill = Y)
+mylist = Listbox(m, yscrollcommand = scrollbar.set)
+
+for line in range(70):
+    if line == 69:
+        mylist.insert(END, "Nice")
+    else:
+        mylist.insert(END, "Line" + str(line))
+
+mylist.pack(side = LEFT, fill = BOTH)
+scrollbar.config(command = mylist.yview)
+
+
 #Everything underneath this is for grid, everything above is pack organizing.
 #They cannot be mixed in the same frame
 
+
 entry_window = Toplevel(m)
 entry_window.title("Grid organized instead of pack")
+
 
 Label(entry_window, text = "Glad").grid(row = 0)
 Label(entry_window, text = "OS").grid(row = 1)
@@ -41,12 +60,13 @@ user_entry_2 = Entry(entry_window)
 user_entry_1.grid(row = 0, column = 1)
 user_entry_2.grid(row = 1, column = 1)
 
+
 check_butt_1 = IntVar()
 Checkbutton(entry_window, text = "Pistachio", variable = check_butt_1).grid(row = 3, sticky = W) 
 check_butt_2 = IntVar()
 Checkbutton(entry_window, text = "Peanut", variable = check_butt_2).grid(row = 4,sticky = W)
 
-# https://www.geeksforgeeks.org/python-gui-tkinter/ - at Radio Button lesson
+# https://www.geeksforgeeks.org/python-gui-tkinter/ - at Output lesson
 
 
 m.mainloop()
