504004650fd04721c9ed8b680838529f5974e80d
paxington181 <tsandrus@rogers.com>
2026-05-27 12:37:05 -0400

    Start of tutorial, setting column and row sizes

--- main.py
+++ main.py
@@ -3,5 +3,18 @@ from tkinter import ttk
 
 main = tk.Tk()
 main.title("Grid Layout Tutorial")
+window.geometry('600x400')
+
+label1 = ttk.Label(main, text = "Label 1", background = "red")
+label2 = ttk.Label(main, text = "Label 2", background = "blue")
+label3 = ttk.Label(main, text = "Label 3", background = "green")
+label4 = ttk.Label(main, text = "Label 4", background = "yellow")
+button1 = ttk.Button(main, text = "Button 1")
+button2 = ttk.Button(main, text = "Button 2")
+entry = ttk.Entry(window)
+
+window.columnconfigure(0, weight = 1)
+window.columnconfigure(1, weight = 1)
+window.rowconfigure(0, weight = 1)
 
 main.mainloop()
