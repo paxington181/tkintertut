76a2561f41c5846ae6444139458863a2ea875e60
paxington181 <tsandrus@rogers.com>
2026-05-27 14:34:18 -0400

    Corrected window name errors

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import ttk
 
 main = tk.Tk()
 main.title("Grid Layout Tutorial")
-window.geometry('600x400')
+main.geometry('600x400')
 
 label1 = ttk.Label(main, text = "Label 1", background = "red")
 label2 = ttk.Label(main, text = "Label 2", background = "blue")
@@ -11,10 +11,13 @@ label3 = ttk.Label(main, text = "Label 3", background = "green")
 label4 = ttk.Label(main, text = "Label 4", background = "yellow")
 button1 = ttk.Button(main, text = "Button 1")
 button2 = ttk.Button(main, text = "Button 2")
-entry = ttk.Entry(window)
+entry = ttk.Entry(main)
 
-window.columnconfigure(0, weight = 1)
-window.columnconfigure(1, weight = 1)
-window.rowconfigure(0, weight = 1)
+main.columnconfigure(0, weight = 1)
+main.columnconfigure(1, weight = 1)
+main.rowconfigure(0, weight = 1)
+
+label1.grid(row = 0, column = 0)
+label2.grid(row = 0, column = 1)
 
 main.mainloop()
